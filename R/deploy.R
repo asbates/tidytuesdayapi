b541d9a6c8c12b0ae78d13420853c216d188b160
Andrew Bates <andrewbates73@gmail.com>
2021-06-16 11:40:58 -0700

    make a few adjustments to the api

--- R/deploy.R
+++ R/deploy.R
@@ -4,24 +4,22 @@
 tt_drop <- analogsea::as.droplet("TidyTuesday")
 plumberDeploy::do_deploy_api(
   droplet = tt_drop,
-  path = "TidyTuesday",
+  path = "api",
   localPath = ".",
-  port = 8002,
-  forward = TRUE,
-  overwrite = TRUE
+  port = Sys.getenv("PORT"),
+  forward = TRUE
 )
 
 # NOTES:
 # 1. .Renviron doesn't deploy (i think because it starts with a '.'?)
-#  so need to add it at var/plumber/TidyTuesday
-#  then reboot with 'sudo reboot now' for changes to take affect
+#  so need to add it at var/plumber/api
 # 2. plumber has a bug where if serving static files at /, docs give a 404
 #  so for now, set docs=FALSE (default). later, can add if desired
 #  see plumber PR #748
-# 3. forward=TRUE points requests for / to /TidyTuesday, where this project
+# 3. forward=TRUE points requests for / to /api, where this project
 #  lives. didn't have that at first. so this is a reminder of why
 #  see https://community.rstudio.com/t/what-does-plumberdeploy-do-forward-do/107304
 # 4. see ?plumberDeploy::do_provision() Details for how to
-#  a. restart plumber: systemctl restart plumber-TidyTuesday
-#  b. see plumber logs: journalctl -u plumber-TidyTuesday
-#     logs from last 5 minutes: journalctl -u plumber-TidyTuesday -S -5m
+#  a. restart plumber: systemctl restart plumber-api
+#  b. see plumber logs: journalctl -u plumber-api
+#     logs from last 5 minutes: journalctl -u plumber-api -S -5m
